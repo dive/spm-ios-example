b11134f8931ce7a2e6f3e0c5d2c1d5b5c2dcb280
dive <artyom.loenko@mac.com>
2019-01-20 11:48:59 +0000

    added an example iOS application

---
+++ ExampleApplication/ExampleApplication/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ExampleApplication
+//
+//  Created by Artem Loenko on 20/01/2019.
+//  Copyright © 2019 Artem Loenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
